73adaf925d4ac1e3b48117116e9033c0ce8d90d0
isaacrooneykoros <isaacrooneykoros@gmail.com>
2025-08-18 00:10:24 +0300

    Fix Order_Details foreign keys formatting

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -38,11 +38,11 @@ CREATE TABLE IF NOT EXISTS Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 -- Order_Details table
-CREATE TABLE IF NOT EXISTS order_details (
-    orderdetailid INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
